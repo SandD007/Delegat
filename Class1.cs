cde223e133d3d94d2fd5d775dcd25730575befe5
Dmytro Kulinich <Spo1091@i.ua>
2023-03-06 13:34:18 +0200

    Add homework 3_2 (maby wrong)

---
+++ Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomethingWithTheDelegates
+{
+    public static class Class1
+    {
+        public static ShowHandler ShowDelegate = Program.Show;
+
+        public delegate void ShowHandler(Class2.ResultHandler t);
+
+        public static int Pow(int firstValue, int secondValue)
+        {
+            int result = firstValue * secondValue;
+            return result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SomethingWithTheDelegates
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Class2.ResultDelegate = Class2.Calc(Class2.PowDelegate, 2, 3);
+            Show(Class2.ResultDelegate);
+        }
+
+        public static void Show(Class2.ResultHandler value)
+        {
+            bool result = value(2);
+
+            Console.WriteLine(result);
+        }
+    }
+}
